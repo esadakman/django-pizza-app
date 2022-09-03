6c24f28b78586043e2fcd3c56212d75271d489f9
esadakman <esadd26@gmail.com>
2022-09-03 15:25:47 +0300

    forms models

--- pizza/models.py
+++ pizza/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Pizza(models.Model):
+    topping_one = models.CharField(("Topping One"), max_length=50)
+    topping_two = models.CharField(("Topping Two"), max_length=50)
+    SIZES = (
+        ("1", "Small"),
+        ("2", "Medium"),
+        ("3", "Large"),
+    )
+    size = models.CharField((""), max_length=50, choices=SIZES)
+
+
+
+    def __str__(self):
+        return f"{self.topping_one}, {self.topping_two}, {self.size}"
