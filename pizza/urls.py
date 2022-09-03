529c0e2175dbefcc86938558cd7e88e4e4707dcc
esadakman <esadd26@gmail.com>
2022-09-03 16:39:42 +0300

    edit

--- pizza/urls.py
+++ pizza/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import form, form_pizza
+from .views import edit, form, form_pizza
 # from 
 
 urlpatterns = [
     path('', form_pizza, name='home'),
-    path('order/', form, name='home'),
+    path('order/', form, name='order'),
+    path('edit/<int:id>', edit, name='edit'),
      
 ]
--- pizza/views.py
+++ pizza/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from pizza.forms import PizzaForm
-
+from .models import Pizza
 
 
 def form_pizza(request):
     form = PizzaForm()
-
+    
     context = {
         'forms': form
     }
@@ -14,8 +14,28 @@ def form_pizza(request):
 
 def form(request):
     form = PizzaForm()
-
+    if request.method == "POST":
+        form = PizzaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # messages.success(request,"Todo created successfully")
+            return redirect("home")
     context = {
         'forms': form
     }
     return render(request, 'pizza/order.html', context)
+
+def edit(request , id ):
+    order = Pizza.objects.get(id=id)
+    form = PizzaForm(instance=order)
+    if request.method == "POST":
+        update_form = PizzaForm(request.POST, instance=order) 
+        if update_form.is_valid():
+            update_form.save()
+            form = update_form
+            # messages.success(request,"Todo created successfully")
+            return redirect("home")
+    context = {
+        'forms': form
+    }
+    return render(request, 'pizza/edit_order.html', context)
