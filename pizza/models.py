3267dbcbc8520a6b316e95941d51b4124f91818a
esadakman <esadd26@gmail.com>
2022-09-04 17:33:54 +0300

    edit order , multiple forms

--- pizza/models.py
+++ pizza/models.py
@@ -4,9 +4,9 @@ class Pizza(models.Model):
     topping1 = models.CharField(("Topping One"), max_length=50)
     topping2 = models.CharField(("Topping Two"), max_length=50)
     SIZES = (
-        ("Small", "small"),
-        ("Medium", "medium"),
-        ("Large", "large"),
+        ("Small", "Small"),
+        ("Medium", "Medium"),
+        ("Large", "Large"),
     )
     size = models.CharField((""), max_length=50, choices=SIZES)
 
